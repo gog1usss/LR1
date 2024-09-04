ce1c5ce780655aeaf4a2fcc06e084feb0bacd1b2
maryn <maryn@JARVIS>
2024-09-04 19:43:47 +0300

    Добавьте файлы проекта.

---
+++ LR1/Examples.cs
@@ -0,0 +1,60 @@
+﻿class Examples
+{
+    static void Main1(string[] args)
+    {
+        Console.WriteLine("Приклад 1:");
+        string s1 = "Dfg + t5 - r7y; asd* s34; rt";
+        int t = s1.IndexOf(";");
+        int i, k = 0;
+        for (i = 0; i <= t; i++)
+        {
+            if (char.IsLetter(s1[i])) k++;
+        }
+        Console.WriteLine(k);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Приклад 3:");
+        string s2 = "АУАУАПАУК";
+        i = 0;
+        do
+        {
+            string ss = s2.Substring(i, 2);
+            if (ss == "АУ")
+            {
+                s2 = s2.Insert(i, "О");
+                i = i + 3;
+            }
+            else
+            {
+                i++;
+            }
+        } while (i < s2.Length - 1);
+        Console.WriteLine(s2);
+
+        Console.WriteLine();
+
+
+        Console.WriteLine("Приклад 3:");
+        string s3 = "бiологiя алгебра iсторiя географiя геометрiя";
+        string c = "i";
+        string[] a;
+        a = s3.Split(' ');
+        for (i = 0; i < a.Length; i++)
+        {
+            t = a[i].IndexOf(c);
+            if (t != -1) Console.WriteLine(a[i]);
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Приклад 4:");
+        string s4 = "бiологiя алгебра iсторiя географiя геометрiя";
+        a = s4.Split(' ');
+        Array.Reverse(a);
+        string s2_reversed = string.Join(" ", a);
+        Console.WriteLine(s2_reversed);
+
+        Console.ReadKey();
+    }
+}
---
+++ LR1/MainTasks.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Task 1:");
+        string NewString = "gjtry4kdlfld6kliok45";
+        Console.WriteLine(NewString);
+        char ChoosenLetter = 'k';
+
+        int count = 0;
+        for (int i = 0; i < NewString.Length; i++)
+        {
+            if (NewString[i] == ChoosenLetter)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Letter '{ChoosenLetter}' appears {count} times in the string");
+
+        Console.WriteLine("Task 2:");
+
+        string CorrectString = "IPZ23theBEST";
+        char Symbol = '-';
+        string ChangedString = "";
+        Console.WriteLine($"String without changes: {CorrectString}");
+
+        for (int i = 0; i < CorrectString.Length; i++)
+        {
+            ChangedString += Symbol + CorrectString[i].ToString();
+        }
+        Console.WriteLine($"String separated by '-': {ChangedString}");
+
+        Console.WriteLine("Task 3:");
+
+        string Start = "Арка Алка тата Анна";
+        string[] words = Start.Split(' ');
+
+        Console.WriteLine("Words that start and end with the same letter:");
+        foreach (string word in words)
+        {
+            string lowerWord = word.ToLower();
+            if (lowerWord[0] == lowerWord[lowerWord.Length - 1])
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        Console.WriteLine("Task 4:");
+        Console.WriteLine("Enter the sentense:");
+        string NewSentence = Console.ReadLine();
+        string[] wordss = NewSentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        string oddWords = "";
+        string evenWords = "";
+        for (int i = 0; i < wordss.Length; i++)
+        {
+            if ((i + 1) % 2 != 0)
+            {
+                oddWords += wordss[i] + " ";
+            }
+            else
+            {
+                evenWords += wordss[i] + " ";
+            }
+        }
+        Console.WriteLine("Words with nonpaired numbers:");
+        Console.WriteLine(oddWords.Trim());
+
+        Console.WriteLine("Words with paired numbers:");
+        Console.WriteLine(evenWords.Trim());
+
+    }
+}
